9c33b7bb64646ab293e2d2b756e55bf6b0fc4d70
Marko Milovanovic <maki.marko.09@gmail.com>
2022-07-23 02:06:31 +0200

    GUI and translations improvements

--- src/main/java/os/arcadiadevs/playerservers/hubcore/utils/GUIUtils.java
+++ src/main/java/os/arcadiadevs/playerservers/hubcore/utils/GUIUtils.java
@@ -4,7 +4,6 @@ import com.cryptomorin.xseries.XMaterial;
 import com.samjakob.spigui.buttons.SGButton;
 import com.samjakob.spigui.item.ItemBuilder;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import os.arcadiadevs.playerservers.hubcore.PSHubCore;
 import os.arcadiadevs.playerservers.hubcore.database.DataBase;
@@ -74,15 +73,16 @@ public class GUIUtils {
     public static void openMenu(Player player) {
 
         final var PSH = PSHubCore.getInstance();
-        final var menu = PSH.getSpiGUI().create(ChatUtil.translate(PSH.getConfig().getString("gui.player-menu.menu.name")), 4);
-
-        menu.setAutomaticPaginationEnabled(false);
-        menu.setBlockDefaultInteractions(true);
 
         new Thread(() -> {
             if (!DataBase.containsServer(player.getUniqueId().toString())) {
+                final var menu = PSH.getSpiGUI().create(ChatUtil.translate(PSH.getConfig().getString("gui.player-menu.menu.name")), 3);
+                menu.setAutomaticPaginationEnabled(false);
+                menu.setBlockDefaultInteractions(true);
+
                 var itemCreate = new ItemBuilder(XMaterial.DIAMOND_BLOCK.parseMaterial())
-                        .name(ChatUtil.translate("&aCreate Server"))
+                        .name(PSH.getConfig().getString("gui.player-menu.menu.create.name"))
+                        .lore(PSH.getConfig().getStringList("gui.player-menu.menu.create.lore"))
                         .build();
 
                 menu.setButton(0, 13, new SGButton(itemCreate).withListener(listener -> BungeeUtil.createServer(listener, player, player.getDisplayName())));
@@ -90,9 +90,21 @@ public class GUIUtils {
                 return;
             }
 
+            final var menu = PSH.getSpiGUI().create(ChatUtil.translate(PSH.getConfig().getString("gui.player-menu.menu.name")), 4);
+            menu.setAutomaticPaginationEnabled(false);
+            menu.setBlockDefaultInteractions(true);
+
+            final var deleteConfirmationMenu = PSH.getSpiGUI().create(ChatUtil.translate(PSH.getConfig().getString("gui.player-menu.menu.delete.confirmation.name")), 3);
+            deleteConfirmationMenu.setAutomaticPaginationEnabled(false);
+            deleteConfirmationMenu.setBlockDefaultInteractions(true);
+
+            final var stopConfirmationMenu = PSH.getSpiGUI().create(ChatUtil.translate(PSH.getConfig().getString("gui.player-menu.menu.stop.confirmation.name")), 3);
+            stopConfirmationMenu.setAutomaticPaginationEnabled(false);
+            stopConfirmationMenu.setBlockDefaultInteractions(true);
+
             final var server = new Server(player);
 
-            var lore = PSH.getConfig().getStringList(server.getServerStatus() == ServerStatus.ONLINE ? "gui.player-menu.menu.online.lore" : "gui.player-menu.menu.offline.lore");
+            var lore = PSH.getConfig().getStringList(server.getServerStatus() == ServerStatus.ONLINE ? "gui.player-menu.menu.info.online.lore" : "gui.player-menu.menu.info.offline.lore");
 
             lore = lore.stream()
                     .map(s -> s.replaceAll("%server%", server.getPlayerName()))
@@ -107,35 +119,68 @@ public class GUIUtils {
                     .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
 
             final var itemPlayer = new ItemBuilder(XMaterial.OAK_SIGN.parseMaterial())
-                    .name(ChatUtil.translate(PSH.getConfig().getString(server.getServerStatus() == ServerStatus.ONLINE ? "gui.player-menu.menu.online.name" : "gui.player-menu.menu.offline.name")
+                    .name(ChatUtil.translate(PSH.getConfig().getString(server.getServerStatus() == ServerStatus.ONLINE ? "gui.player-menu.menu.info.online.name" : "gui.player-menu.menu.info.offline.name")
                             .replaceAll("%server%", server.getPlayerName())))
                     .lore(lore)
                     .build();
 
-            var itemDelete = new ItemBuilder(XMaterial.BARRIER.parseMaterial())
-                    .name("&cDelete Server")
+            final var itemDelete = new ItemBuilder(XMaterial.BARRIER.parseMaterial())
+                    .name(ChatUtil.translate(PSH.getConfig().getString("gui.player-menu.menu.delete.name")))
+                    .lore(PSH.getConfig().getStringList("gui.player-menu.menu.delete.lore"))
+                    .build();
+
+            final var itemQuestionYes = new ItemBuilder(XMaterial.EMERALD_BLOCK.parseMaterial())
+                    .name(ChatUtil.translate(PSH.getConfig().getString("gui.player-menu.menu.delete.confirmation.accept.name")))
+                    .lore(PSH.getConfig().getStringList("gui.player-menu.menu.delete.confirmation.accept.lore"))
+                    .build();
+
+            final var itemQuestionNo = new ItemBuilder(XMaterial.REDSTONE_BLOCK.parseMaterial())
+                    .name(ChatUtil.translate(PSH.getConfig().getString("gui.player-menu.menu.delete.confirmation.decline.name")))
+                    .lore(PSH.getConfig().getStringList("gui.player-menu.menu.delete.confirmation.decline.lore"))
                     .build();
 
             if (server.getServerStatus() == ServerStatus.ONLINE) {
-                var itemJoin = new ItemBuilder(XMaterial.OAK_DOOR.parseMaterial())
-                        .name(ChatUtil.translate("&aJoin Server"))
+                final var itemJoin = new ItemBuilder(XMaterial.OAK_DOOR.parseMaterial())
+                        .name(ChatUtil.translate(PSH.getConfig().getString("gui.player-menu.menu.join.name")))
+                        .lore(PSH.getConfig().getStringList("gui.player-menu.menu.join.lore"))
                         .build();
-                var itemStop = new ItemBuilder(XMaterial.REDSTONE_BLOCK.parseMaterial())
-                        .name(ChatUtil.translate("&cStop Server"))
+                final var itemStop = new ItemBuilder(XMaterial.REDSTONE_BLOCK.parseMaterial())
+                        .name(ChatUtil.translate(PSH.getConfig().getString("gui.player-menu.menu.stop.name")))
+                        .lore(PSH.getConfig().getStringList("gui.player-menu.menu.stop.lore"))
                         .build();
+
                 menu.setButton(0, 22, new SGButton(itemJoin).withListener(listener -> BungeeUtil.connectPlayer(listener, player, player.getDisplayName())));
-                menu.setButton(0, 20, new SGButton(itemStop).withListener(listener -> BungeeUtil.stopServer(listener, player, player.getDisplayName())));
+                menu.setButton(0, 20, new SGButton(itemStop).withListener(listener -> {
+                    stopConfirmationMenu.setButton(0, 11, new SGButton(itemQuestionYes).withListener(listener2 -> {
+                        BungeeUtil.stopServer(listener2, player, player.getDisplayName());
+                        player.closeInventory();
+                    }));
+                    stopConfirmationMenu.setButton(0, 15, new SGButton(itemQuestionNo).withListener(listener2 -> {
+                        player.closeInventory();
+                    }));
+                    player.openInventory(stopConfirmationMenu.getInventory());
+                }));
             }
 
             if (server.getServerStatus() == ServerStatus.OFFLINE) {
-                var itemStart = new ItemBuilder(XMaterial.EMERALD_BLOCK.parseMaterial())
-                        .name(ChatUtil.translate("&aStart Server"))
+                final var itemStart = new ItemBuilder(XMaterial.EMERALD_BLOCK.parseMaterial())
+                        .name(ChatUtil.translate(PSH.getConfig().getString("gui.player-menu.menu.start.name")))
+                        .lore(PSH.getConfig().getStringList("gui.player-menu.menu.start.lore"))
                         .build();
 
                 menu.setButton(0, 20, new SGButton(itemStart).withListener(listener -> BungeeUtil.startServer(listener, player, player.getDisplayName())));
             }
 
-            menu.setButton(0, 24, new SGButton(itemDelete).withListener(listener -> BungeeUtil.deleteServer(listener, player, player.getDisplayName())));
+            menu.setButton(0, 24, new SGButton(itemDelete).withListener(listener -> {
+                deleteConfirmationMenu.setButton(0, 11, new SGButton(itemQuestionYes).withListener(listener2 -> {
+                    BungeeUtil.deleteServer(listener2, player, player.getDisplayName());
+                    player.closeInventory();
+                }));
+                deleteConfirmationMenu.setButton(0, 15, new SGButton(itemQuestionNo).withListener(listener2 -> {
+                    player.closeInventory();
+                }));
+                player.openInventory(deleteConfirmationMenu.getInventory());
+            }));
             menu.setButton(0, 4, new SGButton(itemPlayer));
 
             Bukkit.getScheduler().runTask(PSHubCore.getInstance(), () -> player.openInventory(menu.getInventory()));
