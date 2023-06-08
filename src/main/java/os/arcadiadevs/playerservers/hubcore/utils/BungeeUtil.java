b6677a6bcfd718f133edafd066fd28df4456f625
OpenSource <opensource@thearcadia.xyz>
2023-06-08 21:53:33 +0200

    Fixes

--- src/main/java/os/arcadiadevs/playerservers/hubcore/utils/BungeeUtil.java
+++ src/main/java/os/arcadiadevs/playerservers/hubcore/utils/BungeeUtil.java
@@ -3,6 +3,7 @@ package os.arcadiadevs.playerservers.hubcore.utils;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import os.arcadiadevs.playerservers.hubcore.PsHubCore;
 import os.arcadiadevs.playerservers.hubcore.models.Server;
 
@@ -74,17 +75,5 @@ public class BungeeUtil {
     player.sendPluginMessage(PsHubCore.getInstance(), "BungeeCord", out.toByteArray());
     player.closeInventory();
   }
-
-    public static void getMultinode(InventoryClickEvent event, Player player) {
-        ByteArrayDataOutput out = ByteStreams.newDataOutput();
-
-        try {
-            out.writeUTF("multinode");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        player.sendPluginMessage(PSHubCore.getInstance(), "BungeeCord", out.toByteArray());
-        event.getWhoClicked().closeInventory();
-    }
 
 }
